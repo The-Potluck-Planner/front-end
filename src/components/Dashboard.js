19fd6b7e6b0c9d9ead43a4a711ef2683b20b0939
Ian McElroy <mcelroyian@gmail.com>
2020-05-27 00:18:52 -0500

    login routing

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 export default function Dashboard({name}) {
+    const { push } = useHistory()
+    const logOut = () => {
+        localStorage.removeItem('token')
+        push('login')
+    }
     return (
         <div>
             <h1>Hello {name}</h1>
+            <button onClick={logOut}>Log out</button>
         </div>
     )
 }
