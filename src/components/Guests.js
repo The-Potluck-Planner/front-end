2f04f2b902c8bd3c7d8a9f214ed557c22c1bd83f
Ian McElroy <mcelroyian@gmail.com>
2020-05-29 02:26:25 -0500

    can view invited

--- src/components/Guests.js
+++ src/components/Guests.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { getGuests } from '../store/actions'
 import styled from 'styled-components'
 
 
-function Guests(props){
-
+function Guests(){
     const RsvpIndicator=styled.span`
         height: 20px;
         width:20px;
@@ -13,14 +15,25 @@ function Guests(props){
         ${props=>(props.RSVP===true ? `background-color: green`: `background-color:red`)}
 
     `
-    console.log(props.info.RSVP)
+    const dispatch = useDispatch()
+    const { guests } = useSelector(event => event.user)
+    const { id } = useParams()
+    
+    useEffect(() => {
+        dispatch(getGuests(id))
+    }, [dispatch, id])
 
     return(
 
         <div>
-          <p>{props.info.name}</p>
-          <p> RSVP:</p> 
-           <RsvpIndicator RSVP={props.info.RSVP}></RsvpIndicator>
+            {guests && guests.map(guest => {
+               return (
+                   <>
+                    <p>{guest.name}</p>
+                    <RsvpIndicator RSVP={guest.RSVP}></RsvpIndicator>
+                   </>
+               )
+           })} 
         </div>
     )
 
--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -4,7 +4,16 @@ import {
     LOGIN_SUCCESS,
     REGISTER_START,
     REGISTER_FAILURE,
-    REGISTER_SUCCESS
+    REGISTER_SUCCESS,
+    GET_USERS_START,
+    GET_USERS_SUCCESS,
+    GET_USERS_FAILURE,
+    INVITE_USER_START,
+    INVITE_USER_SUCCESS,
+    INVITE_USER_FAILURE,
+    GET_GUESTS_START,
+    GET_GUESTS_SUCCESS,
+    GET_GUESTS_FAILURE
 } from '../actions'
 
 
@@ -12,7 +21,10 @@ const init = {
     name:'',
     id: 0,
     isValidating: false,
-    errors:''
+    errors:'',
+    isLoading: false,
+    users: [],
+    guests: [] 
 }
 
 export const userReducer = (state = init, action) => {
@@ -29,6 +41,24 @@ export const userReducer = (state = init, action) => {
             return { ...state }
         case REGISTER_FAILURE:
             return { ...state }
+        case GET_USERS_START:
+            return { ...state, isLoading: true}
+        case GET_USERS_SUCCESS:
+            return { ...state, isLoading: false, users: action.payload }
+        case GET_USERS_FAILURE:
+            return { ...state, isLoading: false, errors: action.payload }
+        case INVITE_USER_START:
+            return { ...state, isLoading: true }
+        case INVITE_USER_SUCCESS:
+            return { ...state, isLoading: false}
+        case INVITE_USER_FAILURE:
+            return { ...state, isLoading: false, errors: action.payload}
+        case GET_GUESTS_START:
+            return { ...state, isLoading: true }
+        case GET_GUESTS_SUCCESS:
+            return { ...state, isLoading: false, guests: action.payload }
+        case GET_GUESTS_FAILURE:
+            return { ...state, isLoading: false, errors: action.payload }
         default: 
             return { ...state }
     }
